d32fda58ee7f8c95926aa3e84c1464c36a60f9d6
minh seven <minhvip245@outlook.com>
2020-07-19 14:36:22 +0700

    Add project files.

---
+++ MovieApp/MovieApp/View/WatchlistPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MovieApp.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class WatchlistPage : ContentPage
+    {
+        public WatchlistPage()
+        {
+            InitializeComponent();
+        }
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            moviesColView.SelectedItem = null;
+        }
+    }
+}
---
+++ MovieApp/MovieApp/ViewModel/DetailViewModel.cs
@@ -0,0 +1,31 @@
+﻿using MovieApp.Model;
+using MovieApp.View;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace MovieApp.ViewModel
+{
+    public class DetailViewModel : BaseViewModel
+    {
+        private Movie selectedMovie;
+        public Movie SelectedMovie
+        {
+            get { return selectedMovie; }
+            set
+            {
+                selectedMovie = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand PlayCommand => new Command(() =>
+        {
+            var vm = new PlayerViewModel { SelectedMovie = selectedMovie };
+            var page = new PlayerPage { BindingContext = vm };
+            Application.Current.MainPage.Navigation.PushAsync(page);
+        });
+    }
+}
