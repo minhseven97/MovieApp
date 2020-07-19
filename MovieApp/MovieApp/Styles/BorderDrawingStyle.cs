6af10c3e238c51c79e5328c496a00c991bfa6771
minh seven <minhvip245@outlook.com>
2020-07-19 21:56:42 +0700

    thêm màn hình đăng nhập

---
+++ MovieApp/MovieApp/Styles/BorderDrawingStyle.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace MovieApp.Styles
+{
+    public enum BorderDrawingStyle
+    {
+        Inside,
+        Outside
+    }
+}
---
+++ MovieApp/MovieApp/View/Login.xaml.cs
@@ -0,0 +1,36 @@
+﻿using mo.Animations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MovieApp.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Login : ContentPage
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            Task.Run(async () =>
+            {
+                await ViewAnimations.FadeAnimY(FaceButton);
+                await ViewAnimations.FadeAnimY(LoginButton);
+                await ViewAnimations.FadeAnimY(SignupButton);
+            });
+        }
+
+        private void login_click(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new WatchlistPage());
+        }
+    }
+}
